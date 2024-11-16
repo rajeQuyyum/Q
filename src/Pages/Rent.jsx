6f486277b0cd0afe9dd270bb72c2b8e687e6eb08
Raje <rajevictory@gmail.com>
2024-11-16 05:15:00 -0800

    new changes made

--- src/Pages/Rent.jsx
+++ src/Pages/Rent.jsx
@@ -9,7 +9,7 @@ export default function Rent() {
         <h1 className='font-bold text-3xl mb-5 py-5'>Rent Options</h1>
 
         <div className='flex flex-wrap gap-2 justify-center'>
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2 w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2 w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmZNtSEG63gFzuJG-6N8VK043uPeIl46NazvODmQvpIh_f38UJEJwt-qSRMses4Y3UBFQ&usqp=CAU" alt="" />
                 <h1 className='font-bold text-2xl'>5 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -24,7 +24,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://images.nigeriapropertycentre.com/properties/images/1751714/06512c1a113b15-luxury-and-beautifully-finished-11-bedroom-mansion-with-3-bqs-for-sale-old-ikoyi-ikoyi-lagos.jpg" alt="" />
                 <h1 className='font-bold text-2xl'>11 Bedroom </h1>
                 <div className='flex items-center gap-1'>
@@ -39,7 +39,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://ownhomesltd.com/property/598.jpg" alt="" />
                 <h1 className='font-bold text-2xl'>7 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -55,7 +55,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://luxuryvillas.com.ng/wp-content/uploads/2020/08/5-bedroom.jpeg" alt="" />
                 <h1 className='font-bold text-2xl'>6 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -71,7 +71,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://images.prop24.com/340498798/Crop600x400" alt="" />
                 <h1 className='font-bold text-2xl'>5 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -87,7 +87,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://real-estate-nigeria.beforward.jp/wp-content/uploads/2023/08/349362019_17988635108178704_1056162444269353089_n.jpg" alt="" />
                 <h1 className='font-bold text-2xl'>5 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -103,7 +103,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://lagosabuja.com/wp-content/uploads/classified-listing/2023/01/New_5_Bedroom_Dupex_orchid_Road_Lekki_Lagos-400x400.png" alt="" />
                 <h1 className='font-bold text-2xl'>4 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -119,7 +119,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://real-estate-nigeria.beforward.jp/wp-content/uploads/2022/10/WhatsApp-Image-2022-10-24-at-10.44.07-AM.jpeg" alt="" />
                 <h1 className='font-bold text-2xl'>Detached 5 Bedroom</h1>
                 <div className='flex items-center gap-1'>
@@ -135,7 +135,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://i0.wp.com/nigeriapropertyzone.com.ng/wp-content/uploads/2019/09/IMG-20190917-WA0002.jpg?fit=1008%2C756&ssl=1" alt="" />
                 <h1 className='font-bold text-2xl'>5 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -151,7 +151,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://www.superiteafrica.com/uploads/property/1730897254IMG_0563.jpg" alt="" />
                 <h1 className='font-bold text-2xl'>4 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -168,7 +168,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://images.prop24.com/340846179/Crop600x400" alt="" />
                 <h1 className='font-bold text-2xl'>4 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -184,7 +184,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full  h-52' src="https://images.privateproperty.com.ng/large/4-bedrooms-semi-detached-duplex-with-bq-LFQiwxZANwEztblusKNY.jpg" alt="" />
                 <h1 className='font-bold text-2xl'>4 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -200,7 +200,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full  h-52' src="https://images.prop24.com/344095955/Crop600x400" alt="" />
                 <h1 className='font-bold text-2xl'>4 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -216,7 +216,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full  h-52' src="https://images.propertypro.ng/large/5-bedroom-detached-duplex-for-sale-in-lekki-16may41-57saIh6ThtHMaGWEeUny.jpeg" alt="" />
                 <h1 className='font-bold text-2xl'>4 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
@@ -232,7 +232,7 @@ export default function Rent() {
                 </div>
             </div>
 
-            <div className='bg-slate-100 md:w-[260px] md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
+            <div className='bg-slate-100 md:w-[260px] Buy md:mx-0 mx-2  w-full border border-gray-400 rounded-md flex flex-col gap-2 items-center'>
                 <img className='md:w-[260px] w-full h-52' src="https://images.nigeriapropertycentre.com/properties/images/thumbs/2561604/0672f951191c29-newly-built-contemporary-built-4-bedroom-fully-detached-duplex-with-bq-semi-detached-duplexes-for-sale-ikota-lekki-lagos.jpg" alt="" />
                 <h1 className='font-bold text-2xl'>4 Bedroom Duplex</h1>
                 <div className='flex items-center gap-1'>
