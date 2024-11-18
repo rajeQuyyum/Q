5ab8dbcba57b0da3065965d0758c690325188a69
Raje <rajevictory@gmail.com>
2024-11-18 08:58:07 -0800

    new changes made

--- src/Pages/Det.jsx
+++ src/Pages/Det.jsx
@@ -13,7 +13,7 @@ export default function Det() {
         <div className='flex-wrap flex justify-center gap-5'>
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
-                <h1 className='mb-2'>Living Room</h1>
+                <h1 className='mb-2 text-2xl font-normal'>Living Room</h1>
                 <div className=' mb-3'>
                 <img className=' md:w-[580px] w-full h-[380px]' src="https://st.hzcdn.com/simgs/pictures/living-rooms/modern-living-room-debra-ackerbloom-interiors-llc-img~c971036c0bdc4f53_14-9822-1-a92e8d5.jpg" alt="" />
                 </div>
@@ -21,7 +21,7 @@ export default function Det() {
 
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
-                <h1 className='mb-2'>Room</h1>
+                <h1 className='mb-2 text-2xl font-normal'>Room</h1>
                 <div className=' mb-3'>
                 <img className=' md:w-[580px] w-full h-[380px]' src="https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg" alt="" />
                 </div>
@@ -29,42 +29,50 @@ export default function Det() {
 
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
-                <h1 className='mb-2'>Kitchen</h1>
+                <h1 className='mb-2 text-2xl font-normal'>Kitchen</h1>
                 <div className=' mb-3'>
                 <img className=' md:w-[580px] w-full h-[380px]' src="https://cdn.prod.website-files.com/64886deb8d480dcaed316cb3/65d73bc5d947bcec2ccc2e33_unveiling-the-perfect-fit-what-are-the-dimensions-of-a-kitchen-island.webp" alt="" />
                 </div>
             </div>
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
-                <h1 className='mb-2'>Family </h1>
+                <h1 className='mb-2 text-2xl font-normal'>Family </h1>
                 <div className=' mb-3'>
                 <img className=' md:w-[580px] w-full h-[380px]' src="https://intdesigners.com/wp-content/uploads/2023/01/average-living-room-size-1-1.png" alt="" />
                 </div>
             </div>
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
-                <h1 className='mb-2'>Roof Top</h1>
+                <h1 className='mb-2 text-2xl font-normal'>Roof Top</h1>
                 <div className=' mb-3'>
                 <img className=' md:w-[580px] w-full h-[380px]' src="https://st.hzcdn.com/simgs/6c41a4b50905a7d8_14-1744/_.jpg" alt="" />
                 </div>
             </div>
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
-                <h1 className='mb-2'>Bathroom</h1>
+                <h1 className='mb-2 text-2xl font-normal'>Bathroom</h1>
                 <div className=' mb-3'>
                 <img className=' md:w-[580px] w-full h-[380px]' src="https://cdn.habitusliving.com/wp-content/uploads/2024/02/1.bathroomfeat.jpg" alt="" />
                 </div>
             </div>
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
+            <h1 className='mb-2 text-2xl font-normal'>Clips</h1>
 
-             <img src={vid} alt="" />
+            <div className='mb-3'>
+            <img className='md:w-[580px] w-full h-[380px]' src={vid} alt="" />
+            </div>
             </div>
 
 
 
             <div className='lg:w-[600px] md:w-[390px] Buy  flex flex-col items-center bg-slate-300 md:px-2 px-3 md:mx-0 mx-2'>
-                <img src={vid2} alt="" />
+             <h1 className='mb-2 text-2xl font-normal'>Clips</h1>
+
+             <div className='mb-3'>
+             <img className='md:w-[580px] w-full h-[380px]' src={vid2} alt="" />
+             </div>
+                
 
             </div>
 
