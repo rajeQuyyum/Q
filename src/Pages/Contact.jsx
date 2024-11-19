91c7d2a7b115bdf709efbd8009f9eaaa5f122484
Raje <rajevictory@gmail.com>
2024-11-19 09:27:29 -0800

    new changes made

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -14,10 +14,10 @@ export default function Contact() {
       <input className='border boder h-20 rounded-md px-2' type="text" placeholder='First Name' />
       <input className='border boder h-20 rounded-md px-2' type="text"  placeholder='Last Name'/>
       <input className='border boder h-20 rounded-md px-2' type="text" placeholder='Email' />
-      <textarea className='border boder h-40 rounded-md px-2' name="" id="" placeholder='Message'></textarea>
+      <textarea className='border boder h-40 rounded-md resize-none px-2' name="" id="" placeholder='Message'></textarea>
     </div>
 
-    <button className='bg-blue-600 block m-auto lg:w-[1200px] w-[400px] h-14   rounded-md text-white'>Submit</button>
+    <button className='bg-blue-600 block m-auto lg:w-[1200px] w-[100px] h-14   rounded-md text-white'>Submit</button>
   </div>
   </section>
 
--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -13,19 +13,19 @@ export default function Signin() {
     <h1 className='text-center text-3xl font-semibold pb-16'>Sign In</h1>
 
     <div className='flex flex-col items-center gap-5 mb-3'>
-      <div>
+      <div className='w-[90%] md:w-[700px]'>
       <label className='block' htmlFor="email or phone">Email Or Phone</label>
-      <input className='border border-gray-400 md:w-[500px] w-[400px]  h-12 rounded-md' type="text" name="" id="" />
+      <input className='border border-gray-400 w-full  h-12 rounded-md' type="text" name="" id="" />
       </div>
 
-      <div>
+      <div className='w-[90%] md:w-[700px]'>
       <label className='block ' htmlFor="password">Password</label>
-      <input className='border border-gray-400 md:w-[500px] h-12 w-[400px] rounded-md' type="password" name="" id="" />
+      <input className='border border-gray-400 w-full h-12  rounded-md' type="password" name="" id="" />
       </div>
     </div>
 
     
-    <button className='block m-auto bg-blue-900 md:w-[500px] w-[400px] h-12 rounded-md text-white mb-3'>Login</button>
+    <button className='block m-auto bg-blue-900 md:w-[500px] w-[100px] h-12 rounded-md text-white mb-3'>Login</button>
     
 
 
